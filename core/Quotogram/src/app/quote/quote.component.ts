6613453d81cda84ee9e3bdd67bc1aca42ebc1242
mzazakeith <mzazakeith@gmail.com>
2018-05-26 19:16:12 +0300

    added some placeholder text

--- core/Quotogram/src/app/quote/quote.component.ts
+++ core/Quotogram/src/app/quote/quote.component.ts
@@ -11,11 +11,12 @@ export class QuoteComponent implements OnInit {
 
 
   quotes = [
-    new Quote('My Name', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',new Date(2018,4,25)),
-    new Quote('My Other Name', 'This is my other quote also very inspiring',new Date(2018,4,14)),
-    new Quote('Another Name', 'This is another quote more inspiring',new Date(2020,3,15))
+    new Quote('Ian', '"50 Inspirational Entrepreneurial Quotes" ~Scott Belsky',new Date(2018,4,27)),
+    new Quote('Kierian', '"A year from now you will wish you had started today." ~Karen Lamb',new Date(2018,4,20)),
+    new Quote('Andrew', ' "Choose a job that you like, and you will never have to work a day in your life." ~Confucius',new Date(2018,4,14)),
+    new Quote('Lisa', '"If Plan A doesn’t work, the alphabet has 25 more letters." ~Claire Cook',new Date(2017,3,15)),
+    new Quote('Pri', '"Work hard, be kind, and amazing things will happen." ~Conan O’Brien,',new Date(2017,2,15))
   ]
-
   deleteQuote(isDelete, index) {
     this.quotes.splice(index, 1)
   }
