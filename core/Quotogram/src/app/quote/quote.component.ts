31121645547a81def7189cde18aed10d44b5f524
mzazakeith <mzazakeith@gmail.com>
2018-05-26 14:40:26 +0300

    initial quote component

--- core/Quotogram/src/app/quote/quote.component.ts
+++ core/Quotogram/src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+
+  quotes = [
+    new Quote('My Name', 'This is my quote very inspiring',new Date(2018,4,25)),
+    new Quote('My Other Name', 'This is my other quote also very inspiring',new Date(2018,4,14)),
+    new Quote('Another Name', 'This is another quote more inspiring',new Date(2020,3,15))
+  ]
+
+  deleteQuote(isDelete, index) {
+    this.quotes.splice(index, 1)
+  }
+
+  addNewQuote(quote){
+       this.quotes.push(quote)
+   }
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
