082ed7467c479b108104ea4533435272ae8525f7
mzazakeith <mzazakeith@gmail.com>
2018-05-26 16:26:56 +0300

    delete button functionality added

--- core/Quotogram/src/app/quote-content/quote-content.component.ts
+++ core/Quotogram/src/app/quote-content/quote-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote'
 
 @Component({
@@ -7,52 +7,31 @@ import { Quote } from '../quote'
   styleUrls: ['./quote-content.component.css']
 })
 export class QuoteContentComponent implements OnInit {
+  // upVote: number;
+  // downVote: number;
+
   @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
+  quoteDelete(complete:boolean){
+      this.isComplete.emit(complete);
+    }
 
+  constructor() {
+    // this.upVote = 0;
+    //   this.downVote = 0
+    }
+    //
+    // voteUp(): boolean {
+    //   this.upVote += 1;
+    //   return false;
+    // }
+    //
+    // voteDown(): boolean {
+    //   this.downVote += 1;
+    //   return false;
+    // }
   ngOnInit() {
   }
 
 }
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote'
-//
-// @Component({
-//   selector: 'app-quote-content',
-//   templateUrl: './quote-content.component.html',
-//   styleUrls: ['./quote-content.component.css']
-// })
-// export class QuoteContentComponent implements OnInit {
-//   upVote: number;
-//   downVote: number;
-//
-//   @Input() quote: Quote;
-//   @Output() isComplete = new EventEmitter<boolean>();
-//
-//   quoteDelete(complete:boolean){
-//       this.isComplete.emit(complete);
-//     }
-//
-//     quoteComplete(complete: boolean) {
-//     this.isComplete.emit(complete);
-//   }
-//
-//   constructor() {
-//     this.upVote = 0;
-//       this.downVote = 0
-//     }
-//
-//     voteUp(): boolean {
-//       this.upVote += 1;
-//       return false;
-//     }
-//
-//     voteDown(): boolean {
-//       this.downVote += 1;
-//       return false;
-//     }
-//   ngOnInit() {
-//   }
-//
-// }
