8dc74250e8298075fa96e570d69561bf4af35d53
mzazakeith <mzazakeith@gmail.com>
2018-05-26 18:29:22 +0300

    additional styles added

--- core/Quotogram/src/app/quote-form/quote-form.component.ts
+++ core/Quotogram/src/app/quote-form/quote-form.component.ts
@@ -18,6 +18,8 @@ export class QuoteFormComponent implements OnInit {
 
 
 
+
+
   constructor() { }
 
   ngOnInit() {
--- core/Quotogram/src/app/quote/quote.component.ts
+++ core/Quotogram/src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
 
 
   quotes = [
-    new Quote('My Name', 'This is my quote very inspiring',new Date(2018,4,25)),
+    new Quote('My Name', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',new Date(2018,4,25)),
     new Quote('My Other Name', 'This is my other quote also very inspiring',new Date(2018,4,14)),
     new Quote('Another Name', 'This is another quote more inspiring',new Date(2020,3,15))
   ]
