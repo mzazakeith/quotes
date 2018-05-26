0bda83ce0388555af31254e676eaee5a7e429873
mzazakeith <mzazakeith@gmail.com>
2018-05-26 15:01:16 +0300

    quote content component added and linked

---
+++ core/Quotogram/src/app/quote-content/quote-content.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { Quote } from '../quote'
+
+@Component({
+  selector: 'app-quote-content',
+  templateUrl: './quote-content.component.html',
+  styleUrls: ['./quote-content.component.css']
+})
+export class QuoteContentComponent implements OnInit {
+  @Input() quote: Quote;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import { Quote } from '../quote'
+//
+// @Component({
+//   selector: 'app-quote-content',
+//   templateUrl: './quote-content.component.html',
+//   styleUrls: ['./quote-content.component.css']
+// })
+// export class QuoteContentComponent implements OnInit {
+//   upVote: number;
+//   downVote: number;
+//
+//   @Input() quote: Quote;
+//   @Output() isComplete = new EventEmitter<boolean>();
+//
+//   quoteDelete(complete:boolean){
+//       this.isComplete.emit(complete);
+//     }
+//
+//     quoteComplete(complete: boolean) {
+//     this.isComplete.emit(complete);
+//   }
+//
+//   constructor() {
+//     this.upVote = 0;
+//       this.downVote = 0
+//     }
+//
+//     voteUp(): boolean {
+//       this.upVote += 1;
+//       return false;
+//     }
+//
+//     voteDown(): boolean {
+//       this.downVote += 1;
+//       return false;
+//     }
+//   ngOnInit() {
+//   }
+//
+// }
