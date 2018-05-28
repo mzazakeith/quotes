a046c566f73d50e4da4735afb2f18a850437f519
mzazakeith <mzazakeith@gmail.com>
2018-05-28 09:08:21 +0300

    date count string error fixed

--- core/Quotogram/src/app/date-count.pipe.ts
+++ core/Quotogram/src/app/date-count.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any): number {
+  transform(value: any){
     let today:Date = new Date();
     let noTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
     var dateDifference =Math.abs(value-noTime )
@@ -14,9 +14,9 @@ export class DateCountPipe implements PipeTransform {
     var dateDifferenceSeconds=dateDifference*0.001;
 
     var dateCounter = dateDifferenceSeconds/secondsInADay;
-
+    console.log(dateCounter.toString())
     if (dateCounter >= 1){
-        return (dateCounter +" "+ "Day(s) ago")
+        return (dateCounter.toString() +" Day(s) ago")
     }else if (dateCounter <= 1){
         return ("Today");
     }
